f4ad58b7d2df487dfd4a1c3296f937792921d938
Formation14 <paveltis@tut.by>
2021-06-07 11:45:50 +0300

    ver1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,25 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pojo.Cat;
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(helloWorld1.getMessage());
+        System.out.println(cat1.getName());
+
+        System.out.println(helloWorld1 == helloWorld2);
+        System.out.println("---------------");
+        System.out.println(cat1 == cat2);
+
+        System.out.println(Cat.class);
     }
 }
